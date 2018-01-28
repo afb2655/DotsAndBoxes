507e326ced53951b1038d71d85800e1bb196b648
theaiden28 <afb2655@g.rit.edu>
2018-01-28 11:00:06 -0500

    Makes lines work

--- src/game/Lines.java
+++ src/game/Lines.java
@@ -1,21 +1,40 @@
 package game;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 
 public class  Lines {
     private java.util.ArrayList<Line> lines;
 
     public Lines(int rows, int columns, Dot[][] dots) {
+        ArrayList BruceU = new ArrayList();
         for (int row = 0;row<=rows;row++){
-            for(int c=0; c<=columns; c++){
-
+            for(int columb=0; columb<columns; columb++){
+               Line ugandan_commando = new Line(dots[row][columb],dots[row][columb+1]);
+               BruceU.add(ugandan_commando);
             }
 
         }
+        for (int cawlum = 0; cawlum <= columns; cawlum ++){
+            for (int HellRow = 0; HellRow < rows; HellRow ++){
+                Line tigermafia = new Line(dots[HellRow][cawlum],dots[HellRow+1][cawlum]);
+                BruceU.add(tigermafia);
+            }
+        }
+        this.lines = BruceU;
     }
 
     public Line getLine(int row1, int column1, int row2, int column2) {
-        return this.lines.get(row1);
+        Line lmao = null;
+        for(int i =0; i<this.lines.size(); i++){
+            if ((this.lines.get(i).getFirst().getRow() == row1) && (this.lines.get(i).getFirst().getColumn() == column1)){
+                if ((this.lines.get(i).getSecond().getRow() == row2) && (this.lines.get(i).getSecond().getColumn() == column2)){
+                    return(this.lines.get(i));
+                }
+            }
+        }
+        return lmao;
     }
 
     public int size(){

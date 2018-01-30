85f7331470eaddc0c7a4276562dfa1346cbded85
theaiden28 <31458159+theaiden28@users.noreply.github.com>
2018-01-29 21:02:13 -0500

    Comments

--- src/game/Line.java
+++ src/game/Line.java
@@ -1,7 +1,14 @@
+/**
+ * Author: Aiden Buchheister
+ */
+
 package game;
 
 import java.util.ArrayList;
 
+/**
+ * Line is the line for the boxes and stuff, yo
+ */
 public class Line {
 
 
@@ -15,7 +22,11 @@ public class Line {
     private Dot second;
     public static String VERT_LINE = "|";
 
-
+    /**
+     * Line is the constructor for the Line class
+     * @param first first is the first dot of the line
+     * @param second second is the second dot of the line
+     */
     public Line(Dot first, Dot second) {
         if ((second.getRow() - first.getRow() > 1 || (second.getColumn() - first.getColumn() > 1))){
             throw new AssertionError("Blyat those are wrong coordinates cyka");
@@ -29,6 +40,11 @@ public class Line {
 
     }
 
+    /**
+     * equals checks if one line is equal to another line
+     * @param other other is the OTHER line to check with this
+     * @return returns TRUE or FALSE
+     */
     public boolean equals(Object other){
         if (other instanceof Line){
                 if ((this.first.equals(((Line) other).first) && this.second.equals(((Line) other).second)) || (this.first.equals(((Line) other).second) && this.second.equals(((Line) other).first))) {
@@ -43,7 +59,10 @@ public class Line {
             }
         }
 
-
+    /***
+     * claim claims a line for its owner, GLORY TO THE MOTHERLAND!
+     * @param owner the owner to claim the box, either Glorious Russian or Gross Prussian
+     */
     public void claim(Player owner){
         this.owner = owner;
         for(int i =0; i<boxes.size(); i++){
@@ -51,29 +70,42 @@ public class Line {
         }
     }
 
-
+    /**
+     * adds box
+     * @param box adds a box to this.boxes
+     */
     public void setBox(Box box){
         this.boxes.add(box);
     }
 
-
-    public java.util.ArrayList<Box> getboxes(){
-
-        return(boxes);
-    }
-
+    /**
+     * gets the owner of a line
+     * @return returns the owner of the line
+     */
     public Player getOwner(){
         return (this.owner);
     }
 
+    /**
+     * gets first point of the line
+     * @return returns the first dot
+     */
     public Dot getFirst() {
         return this.first;
     }
 
+    /**
+     * gets the second dot from a line
+     * @return returns the second dot
+     */
     public Dot getSecond() {
         return this.second;
     }
 
+    /**
+     * checks if the line has an owner
+     * @return TRUE or FALSE
+     */
     public  boolean hasOwner(){
         if (this.owner.getLabel() == " ") {
             return false;
@@ -81,12 +113,12 @@ public class Line {
             return true;
         }
         }
-//        for( int i=0; i<this.boxes.size();i++){
-//            if this.boxes(i) ==
-//        }
 
 
-
+    /**
+     * makes a string outta the line
+     * @return returns the string
+     */
     public String toString(){
         if (this.owner == Player.NONE){
             return EMPTY;
--- src/game/Lines.java
+++ src/game/Lines.java
@@ -1,3 +1,8 @@
+/**
+ * Author: Aiden Buchheister
+ */
+
+
 package game;
 
 import org.junit.Test;
@@ -7,6 +12,15 @@ import java.util.ArrayList;
 public class  Lines {
     private java.util.ArrayList<Line> lines;
 
+    /**
+     * Constructs the ArrayList of Lines using muh double for loop algorithms
+     * using BRUCE-U AND THE TIGER MAFIA, COMMANDO, COMMANDOOOOOOOOOOO
+     * Also whoever is reading this should really watch "Who killed captain alex" It's this hilarious ultra low budget
+     * ($200) Ugandan action movie, like its hilarious, and the knuckles meme is based on it
+     * @param rows the rows in the game to construct the line with
+     * @param columns the # of columns to construct
+     * @param dots the 2d array of dots and columns
+     */
     public Lines(int rows, int columns, Dot[][] dots) {
         ArrayList BruceU = new ArrayList();
         for (int row = 0;row<=rows;row++){
@@ -25,6 +39,14 @@ public class  Lines {
         this.lines = BruceU;
     }
 
+    /**
+     * gets a line from the arraylist of lines
+     * @param row1 the first row coordinate
+     * @param column1 the first column coordinate
+     * @param row2 the 2nd row coordinate
+     * @param column2 the 2nd column coordinate
+     * @return returns a line
+     */
     public Line getLine(int row1, int column1, int row2, int column2) {
         Line lmao = null;
         for(int i =0; i<this.lines.size(); i++){
@@ -37,6 +59,10 @@ public class  Lines {
         return lmao;
     }
 
+    /**
+     * gets the size of the arraylist
+     * @return returns the size boi
+     */
     public int size(){
         return this.lines.size();
     }
